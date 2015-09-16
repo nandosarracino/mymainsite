4381b252155acf2f39bcb8764240a8b46632d600
nandosarracino <nando_sarracino20@yahoo.com>
2015-09-15 20:09:01 -0600

    First commit

---
+++ mainsite/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+#using generics
+from django.views.generic import TemplateView
+
+# Create your views here.
+
+class HomeView(TemplateView):
+	template_name = 'home.html'
+		
